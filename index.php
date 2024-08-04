086e430fff623d45eb0a4a037b00c4a74c72b0c0
Fabio Lopes <nunes-lopes@hotmail.com>
2024-08-04 16:52:01 -0300

    Add validate

--- index.php
+++ index.php
@@ -5,55 +5,32 @@ require_once __DIR__ . '/variables.php';
 require_once __DIR__ . '/getPlayersName.php';
 require_once __DIR__ . '/buildBoard.php';
 require_once __DIR__ . '/showBoard.php';
+require_once __DIR__ . '/isPositionCorrect.php';
+require_once __DIR__ . '/validate.php';
 
 do {
     $players = getPlayuersName();
     $player = PLAYER_ONE_ICON;
-
     $board = buildBoard();
 
     $winner = null;
+
     while ($winner === null) {
         echo showBoard($board);
+        $position = (int) readline("Player {$player}, digite a sua posição: ");
 
-        $position = (int) readline("Player {player}, digite a sua posição: ");
-
-        if (!in_array($position, [0, 1, 2, 3, 4, 5, 6, 7, 8])) {
-            echo "\nPosição inexistente, digite novamente.\n";
+        if (!isPositionCorrect($position, $board)){
             continue;
         }
-
-        if ($board[$position] !== '.') {
-            echo "\nPosição ocupada, digite novamente.\n";
-            continue;
-        }
-
+        
         $board[$position] = $player;
 
-        if (
-            ($board[0] === 'X' && $board[1] === 'X' && $board[2] === 'X') ||
-            ($board[3] === 'X' && $board[4] === 'X' && $board[5] === 'X') ||
-            ($board[6] === 'X' && $board[7] === 'X' && $board[8] === 'X') ||
-            ($board[0] === 'X' && $board[3] === 'X' && $board[6] === 'X') ||
-            ($board[1] === 'X' && $board[4] === 'X' && $board[7] === 'X') ||
-            ($board[2] === 'X' && $board[5] === 'X' && $board[8] === 'X') ||
-            ($board[0] === 'X' && $board[4] === 'X' && $board[8] === 'X') ||
-            ($board[2] === 'X' && $board[4] === 'X' && $board[6] === 'X')
-        ) {
-            $winner = 'X';
-        }
-
-        if (
-            ($board[0] === 'O' && $board[1] === 'O' && $board[2] === 'O') ||
-            ($board[3] === 'O' && $board[4] === 'O' && $board[5] === 'O') ||
-            ($board[6] === 'O' && $board[7] === 'O' && $board[8] === 'O') ||
-            ($board[0] === 'O' && $board[3] === 'O' && $board[6] === 'O') ||
-            ($board[1] === 'O' && $board[4] === 'O' && $board[7] === 'O') ||
-            ($board[2] === 'O' && $board[5] === 'O' && $board[8] === 'O') ||
-            ($board[0] === 'O' && $board[4] === 'O' && $board[8] === 'O') ||
-            ($board[2] === 'O' && $board[4] === 'O' && $board[6] === 'O')
-        ) {
-            $winner = 'O';
+        if (validate($board, PLAYER_ONE_ICON)) {
+            $winner = PLAYER_ONE_ICON;
+        } elseif (validate($board, PLAYER_TWO_ICON)) {
+            $winner = PLAYER_TWO_ICON;
+        } else {
+            $winner = null;
         }
 
         if (!in_array('.', $board)) {
